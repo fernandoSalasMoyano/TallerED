532e232de05683af696d136d38b6c98c459bda8f
fernandoSalasMoyano <fernandosalasmoyano@gmail.com>
2023-11-30 12:38:39 +0100

    Primer commit

---
+++ src/es/studium/holaMundo/HolaMundo.java
@@ -0,0 +1,14 @@
+package es.studium.holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!");
+		System.out.print("Buenas!");
+		System.out.println();
+		System.out.println("Buenos dias");
+	
+	}
+
+}
---
+++ src/es/studium/holaMundo/PruebaClase.java
@@ -0,0 +1,5 @@
+package es.studium.holaMundo;
+
+public class PruebaClase {
+
+}
